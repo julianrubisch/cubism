d1be8dbd7a7d2cd47ad8e6ad27ea21d08312c6a7
Julian Rubisch <julian@julianrubisch.at>
2021-10-08 10:44:48 +0200

    Standardize

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Specify your gem's dependencies in cubism.gemspec.
 gemspec
 
 group :development do
-  gem 'sqlite3'
+  gem "sqlite3"
 end
 
-gem 'pry', group: [:development, :test]
+gem "pry", group: [:development, :test]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,6 @@ require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 
-
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
