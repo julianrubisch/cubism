f7f1764257224c06d512f9be1fd06a92fcde5551
Julian Rubisch <julian@julianrubisch.at>
2021-12-15 10:40:42 +0100

    Use ActiveSupport::Digester to produce element ids

--- app/helpers/cubism_helper.rb
+++ app/helpers/cubism_helper.rb
@@ -3,16 +3,16 @@ module CubismHelper
 
   def cubicle_for(resource, user, html_options: {}, appear_trigger: :connect, disappear_trigger: nil, trigger_root: nil, exclude_current_user: true, &block)
     key = "#{block.source_location.join(":")}:#{resource.to_gid}:#{user.to_gid}"
-    verifiable_id = CableReady.signed_stream_verifier.generate(key)
+    digested_id = ActiveSupport::Digest.hexdigest(key)
 
-    Cubism.store[verifiable_id] = Cubism::BlockStoreItem.new(context: self, block: block.dup)
+    Cubism.store[digested_id] = Cubism::BlockStoreItem.new(context: self, block: block.dup)
     tag.cubicle_element(
       identifier: signed_stream_identifier(resource.to_gid.to_s),
       user: user.to_sgid.to_s,
       "appear-trigger": appear_trigger,
       "disappear-trigger": disappear_trigger,
       "trigger-root": trigger_root,
-      id: "cubicle-#{verifiable_id}",
+      id: "cubicle-#{digested_id}",
       "exclude-current-user": exclude_current_user,
       **html_options
     )
