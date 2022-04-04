fffcc3df458e88081fee66ddcebe65c6c5bff2db
Julian Rubisch <julian@julianrubisch.at>
2022-04-04 17:34:22 +0200

    Add sprockets and importmap setup

--- lib/cubism/engine.rb
+++ lib/cubism/engine.rb
@@ -4,5 +4,25 @@ module Cubism
       Cubism.block_store = Cubism::CubicleStore.new("cubism-blocks")
       Cubism.source_store = Cubism::CubicleStore.new("cubism-source")
     end
+
+    initializer "cubism.assets" do |app|
+      if app.config.respond_to?(:assets)
+        app.config.assets.precompile += %w[
+          cubism.js
+          cubism.min.js
+          cubism.min.js.map
+          cubism.umd.js
+          cubism.umd.min.js
+          cubism.umd.min.js.map
+        ]
+      end
+    end
+
+    initializer "cubism.importmap", before: "importmap" do |app|
+      if app.config.respond_to?(:importmap)
+        app.config.importmap.paths << Engine.root.join("lib/cubism/importmap.rb")
+        app.config.importmap.cache_sweepers << Engine.root.join("app/assets/javascripts")
+      end
+    end
   end
 end
---
+++ lib/cubism/importmap.rb
@@ -0,0 +1,2 @@
+pin "cable_ready", to: "cable_ready.min.js", preload: true
+pin "cubism", to: "cubism.min.js", preload: true
